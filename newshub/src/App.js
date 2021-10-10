9d98d185b605cfe5f7fa841e1aad75a2f403ac39
TariqT777 <TariqThomas777@gmail.com>
2021-10-10 16:27:32 -0500

    Added new navbar and homepage components.

--- newshub/src/App.js
+++ newshub/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';  
 
 function App() {
+  //const title = "Welcome to the NewsHub!";
+
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>Testing for Tariq</h1>
+        <Home />
       </div>
     </div>
   );
---
+++ newshub/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The NewsHub</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Technology</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
