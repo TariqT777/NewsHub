e17f3cdcdb034aa0a80f34ba82265a0c7ed57053
TariqT777 <TariqThomas777@gmail.com>
2021-10-11 08:29:06 -0500

    Added more CSS and more sections in the navbar.

--- newshub/src/Navbar.js
+++ newshub/src/Navbar.js
@@ -4,7 +4,41 @@ const Navbar = () => {
             <h1>The NewsHub</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">Technology</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor:"#f1356d",
+                    borderRadius: '8px'
+                }}>Technology</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor:"#f1356d",
+                    borderRadius: '8px'
+                }}>Sports</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor:"#f1356d",
+                    borderRadius: '8px'
+                }}>Politics</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor:"#f1356d",
+                    borderRadius: '8px'
+                }}>World</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor:"#f1356d",
+                    borderRadius: '8px'
+                }}>Entertainment</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor:"#f1356d",
+                    borderRadius: '8px'
+                }}>BlackLivesMatter</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor:"#f1356d",
+                    borderRadius: '8px'
+                }}>Satire</a>
             </div>
         </nav>
      );
