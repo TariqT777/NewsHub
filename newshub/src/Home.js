bbaa7462f7a1ceeb982fdded859c087ba624f70b
TariqT777 <TariqThomas777@gmail.com>
2021-10-11 08:53:16 -0500

    Added a useState webhook tied to the button. Just for testing purposes and will be removed shortly.

--- newshub/src/Home.js
+++ newshub/src/Home.js
@@ -1,12 +1,17 @@
+import {useState} from 'react';
+
 const Home = () => {
 
+    const [name, setName]= useState('Testing');
+
     const handleClick = () =>{
-        console.log('Testing');
+        setName('It worked!')
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
             <button onClick={handleClick}>Click me</button>
         </div>
      );
