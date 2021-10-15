469ad2e98f5998539b32313f892be8052a2685fc
TariqT777 <TariqThomas777@gmail.com>
2021-10-15 17:57:35 -0500

    Changed minor styling and moved come inline css to stylesheet for more readable code.

--- newshub/src/Navbar.js
+++ newshub/src/Navbar.js
@@ -3,42 +3,17 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>The NewsHub</h1>
             <div className="links">
-                <a href="/">Home</a>
+                <a href="/create">Home</a>
+                <a href="/create">Technology</a>
                 <a href="/create" style={{
                     color: "white",
-                    backgroundColor:"#f1356d",
-                    borderRadius: '8px'
-                }}>Technology</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor:"#f1356d",
                     borderRadius: '8px'
                 }}>Sports</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor:"#f1356d",
-                    borderRadius: '8px'
-                }}>Politics</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor:"#f1356d",
-                    borderRadius: '8px'
-                }}>World</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor:"#f1356d",
-                    borderRadius: '8px'
-                }}>Entertainment</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor:"#f1356d",
-                    borderRadius: '8px'
-                }}>BlackLivesMatter</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor:"#f1356d",
-                    borderRadius: '8px'
-                }}>Satire</a>
+                <a href="/create" >Politics</a>
+                <a href="/create">World</a>
+                <a href="/create">Entertainment</a>
+                <a href="/create">BlackLivesMatter</a>
+                <a href="/create">Satire</a>
             </div>
         </nav>
      );
