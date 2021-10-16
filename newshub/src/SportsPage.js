783e67c18f6914c1cb76ebb4a7aa796473d1dda3
TariqT777 <TariqThomas777@gmail.com>
2021-10-16 16:36:33 -0500

    Sports page added, but displays tech articles.

---
+++ newshub/src/SportsPage.js
@@ -0,0 +1,14 @@
+import TechAPI from './TechAPI';
+
+
+
+const Tech = () => {
+    return ( 
+        <div className="Tech">
+            <h2>Techpage</h2>
+            <TechAPI />
+
+        </div>
+     );
+}
+export default Tech;
